7729829c80dfafb48d567ab6b55fc34c2d59839d
Arif <mokapos_arif@Arifs-MacBook-Pro.local>
2018-03-22 01:39:28 +0700

    change image on affiliations page, add home icon on navigation when it's sticky mode

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,10 +1,16 @@
 // JavaScript Document
- 
+
   $(document).ready(function(){
   	"use strict";
 	 if( $( window ).width() >= "768" ) {
-		$(".header-wrapper").sticky({topSpacing:0});	
+		$(".header-wrapper").sticky({topSpacing:0});
     }
+    $('.header-wrapper').on('sticky-start', function() {
+      $('#li-home').css("display","block");
+    });
+     $('.header-wrapper').on('sticky-end', function() {
+       $('#li-home').css("display","none");
 
+
+     });
   });
-  
